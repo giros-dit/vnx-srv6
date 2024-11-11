5c229e9fdd534f94c8060d445948501da05e24bf
Alejandro Villaseca <avillaseca2001@gmail.com>
2024-11-11 09:55:21 +0100

    fix python script

--- EscenarioAcross/createjson.py
+++ EscenarioAcross/createjson.py
@@ -12,8 +12,10 @@ else:
     config = {}
 
 def main():
+    # If exists the conf JSON file and not the old one, create the old one
     if os.path.exists(json_file_path) and not os.path.exists(json_file_path_old):
         os.system("cp VLANtunnels.json VLANtunnelsold.json")
+    # Update the old JSON file with the new one
     elif os.path.exists(json_file_path) and os.path.exists(json_file_path_old):
         with open('VLANtunnelsold.json', 'r') as old_file:
             old_config = json.load(old_file)
@@ -36,30 +38,30 @@ def main():
             print("JSON content deleted")
             continue
 
-        # Preguntar si el camino de vuelta es el mismo que el de ida
+        # Ask if the path is the same
         same_path_input = input(" DOes the package follows the same path between gnBX and UPF? (t if True, any other key if False): ")
         same_path = same_path_input.lower() == 't'
 
-        # Pedir las direcciones fw
+        # Ask for the routers identifiers for forward path
         fw_routers_input = input("Introduce the identifiers separated by spaces, in the same order that they would follow from UPF to GNB: ")
         fw_routersid = [fw.strip() for fw in fw_routers_input.split(' ') if fw.strip()]
         fw_routersip = [f'fcff:{fwid}::1' for fwid in fw_routersid] if fw_routersid else []
 
-        # Si el camino de vuelta no es el mismo, pedir las direcciones rt
+        # If the path is not the same, ask for the routers identifiers for return path
         rt_list = []
         if not same_path:
             rt_routers_input = input("Introduce the identifiers separated by spaces, in the same order that they would follow from GNB to UPF: ")
             rt_routersid = [rt.strip() for rt in rt_routers_input.split(' ') if rt.strip()]
             rt_list = [f'fcff:{rtid}::1' for rtid in rt_routersid] if rt_routersid else []
 
-        # Actualizar o crear el JSON
+        # Create json object
         config[vlan] = {
             "fw": fw_routersip,
             "rt": rt_list,
             "same_path": same_path
         }
 
-        # Guardar el JSON actualizado
+        # Save object in the JSON file
         with open(json_file_path, 'w') as f:
             json.dump(config, f, indent=4)
 
