57a12d7d82688c815e8961d71429699fe5afec7b
alex <a.villasecam@alumnos.upm.es>
2025-02-03 13:08:14 +0100

    updateclabb5g

--- clabernetesAcross/test6nodes/createVLANScni.sh
+++ clabernetesAcross/test6nodes/createVLANScni.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Namespace en Kubernetes
-NS="c9s-srv6"
+NS="across-tc32"
 
 # Lista de VLANs
-VLANS=(1012 1023 1014 1034 2011 2032)
+VLANS=(1001 1002 1003 1004 1005 1006 1007 1008 2001 2002)
 
 # Iterar sobre cada VLAN y crear la definición
 for VLAN in "${VLANS[@]}"; do
