a00cc258fdd7a90a0775591fed2c75b81702236f
alex <a.villasecam@alumnos.upm.es>
2025-02-13 12:56:14 +0100

    Controllerworking

--- clabernetesAcross/test6nodes/src/conf/script.sh
+++ clabernetesAcross/test6nodes/src/conf/script.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+kubectl exec -n across-tc32 deploy/ru -- docker exec ru ip -6 route add fd00:0:2::/127 encap seg6 mode encap segs fcff:2::1,fcff:1::1,fcff:6::1 dev eth1.111
+kubectl exec -n across-tc32 deploy/rg -- docker exec rg ip -6 route add fd00:0:1::/127 encap seg6 mode encap segs fcff:1::1,fcff:2::1,fcff:5::1 dev eth1.111
--- clabernetesAcross/test6nodes/src/tunnelmaker.py
+++ clabernetesAcross/test6nodes/src/tunnelmaker.py
@@ -24,8 +24,8 @@ ID_to_IP = {
     "r2": "fcff:2::1",
     "r3": "fcff:3::1",
     "r4": "fcff:4::1",
-    "ru": "fcff:6::1",
-    "rg": "fcff:5::1"
+    "ru": "fcff:5::1",
+    "rg": "fcff:6::1"
 }
 
 def iniciafichero():
@@ -35,47 +35,43 @@ def iniciafichero():
         f.write("#!/bin/bash\n")
 
 
-def load_config_from_mongo():
-    """Recupera la configuración desde Mongo.
-       Se espera que cada documento tenga las claves 'vlan', 'routers' y 'version'."""
-    client = pymongo.MongoClient("mongodb://mongo:27017/")
-    db = client["across"]
-    routes_col = db["routes"]
-
-    config = {}
-    for doc in routes_col.find():
-        vlan = str(doc.get("vlan"))
-        routers = doc.get("routers", [])
-        version = doc.get("version", 1)
-        config[vlan] = {"routers": routers, "version": version}
-    return config
 
 def createupf(vlan, routers, method):
     VLAN = int(vlan)
+    if routers:
+        routers = routers[1:]
     if 110 <= VLAN < 114:
-        segments = routers + ['fcff:5::1'] if routers else ['fcff:5::1']
+        if routers:
+            segments = [ID_to_IP.get(r, f"unknown_{r}") for r in routers]
+        else:
+            segments = [ID_to_IP['rg']]
     else:
         print("VLAN not supported")
         sys.exit(1)
-    
+
     ipgNB = VLAN_IPv6_gNB.get(VLAN, "unknown")
-    comandoupf = (f"kubectl exec -n {ns} deploy/rupf -- docker exec rupf "
+    comandoupf = (f"kubectl exec -n {ns} deploy/ru -- docker exec ru "
                   f"ip -6 route {method} {ipgNB} encap seg6 mode encap segs "
-                  f"{','.join(segments)} dev eth2.{VLAN}")
+                  f"{','.join(segments)} dev eth1.{VLAN}")
     return comandoupf
 
 def creategnb(vlan, routers, method):
     VLAN = int(vlan)
+    if routers:
+        routers = routers[::-1][1:]
     if 110 <= VLAN < 114:
-        segments = routers + ['fcff:6::1'] if routers else ['fcff:6::1']
+        if routers:
+            segments = [ID_to_IP.get(r, f"unknown_{r}") for r in routers]
+        else:
+            segments = [ID_to_IP['ru']]
     else:
         print("VLAN not supported")
         sys.exit(1)
     
     ipUPF = VLAN_IPv6_UPF.get(VLAN, "unknown")
-    comandognb = (f"kubectl exec -n {ns} deploy/rgnb -- docker exec rgnb "
+    comandognb = (f"kubectl exec -n {ns} deploy/rg -- docker exec rg "
                   f"ip -6 route {method} {ipUPF} encap seg6 mode encap segs "
-                  f"{','.join(segments)} dev eth2.{VLAN}")
+                  f"{','.join(segments)} dev eth1.{VLAN}")
     return comandognb
 
 def save(command, file):
@@ -91,23 +87,19 @@ def main():
     iniciafichero()
     vlan = sys.argv[1]
     version = int(sys.argv[2])
-    routers = json.loads(sys.argv[3])
+    routersid = json.loads(sys.argv[3])
 
-    for vlan, data in config.items():
-        routers = data.get("routers", [])
-        routers = [f"fcff:{router}::1" for router in data.get("routers", [])]
-        version = data.get("version", 1)
-        method = determine_method(version, routers)
-        print(f"Processing VLAN: {vlan} (version {version}) con método: {method}")
+    method = determine_method(version, routersid)
+    print(f"Processing VLAN: {vlan} (version {version}) con método: {method}")
 
-        commandrupf = createupf(vlan, routers, method)
-        comandognb = creategnb(vlan, routers, method)
-        print(f"Command for rupf: {commandrupf}")
-        print(f"Command for rgnb: {comandognb}")
-
-        scriptfile = './conf/script.sh'
-        save(commandrupf, scriptfile)
-        save(comandognb, scriptfile)
+    commandrupf = createupf(vlan, routersid, method)
+    comandognb = creategnb(vlan, routersid, method)
+    print(f"Command for rupf: {commandrupf}")
+    print(f"Command for rgnb: {comandognb}")
+        
+    scriptfile = './conf/script.sh'
+    save(commandrupf, scriptfile)
+    save(comandognb, scriptfile)
 
     print("\nFichero de script generado en el volumen compartido.")
     print("Ejecuta los scripts en el host, por ejemplo:")
