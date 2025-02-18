7eb51f8a0f57a89c5f4be0fa627e79bb022141af
alex <a.villasecam@alumnos.upm.es>
2025-02-18 11:10:13 +0100

    Guardando cambios en rgru

--- clabernetesAcross/test6nodes/conf/rupf/0_conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ip link add link eth1 name eth1.111 type vlan id 111
-ip link add link eth1 name eth1.112 type vlan id 112
-ip link add link eth1 name eth1.113 type vlan id 113
-ip link add link eth2 name eth2.111 type vlan id 111
-ip link add link eth2 name eth2.112 type vlan id 112
-ip link add link eth2 name eth2.113 type vlan id 113
