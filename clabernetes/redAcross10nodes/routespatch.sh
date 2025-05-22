bdf4ba6aad2f61815c8a26dbadb242bc5f23bfdc
alex <a.villasecam@alumnos.upm.es>
2025-05-22 16:27:03 +0200

    update2205

--- clabernetes/redAcross10nodes/routespatch.sh
+++ clabernetes/redAcross10nodes/routespatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl exec deploy/rg1 -- docker exec rg1 ip -6 route add fd00:0:1::/127 encap seg6 mode encap segs fcff:13::1 dev eth1
-kubectl exec deploy/rg2 -- docker exec rg2 ip -6 route add fd00:0:1::/127 encap seg6 mode encap segs fcff:13::1 dev eth1
+kubectl exec deploy/rg -- docker exec rg1 ip -6 route add fd00:0:1::/127 encap seg6 mode encap segs fcff:13::1 dev eth1
+kubectl exec deploy/rc -- docker exec rg2 ip -6 route add fd00:0:1::/127 encap seg6 mode encap segs fcff:13::1 dev eth1
